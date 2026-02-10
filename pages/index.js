59667cc59cd116878ec20be3b8d2d5bc7731b539
Douglas Miguel <106604936+Deglas021@users.noreply.github.com>
2026-02-10 03:35:49 +0000

    adicionei mais enfase hehe

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home(){
     return <h1>Eu queria dizer que quem sempre me apoiou foi voce,
-         e como o felipe pede pra homenagiar alguem... esse alguem pra mim e voce emy, TE AMO!!</h1>
+         e como o felipe pede pra homenagiar alguem... esse alguem pra mim e voce emy, TE AMO!!!!</h1>
 }
 
 export default Home;
